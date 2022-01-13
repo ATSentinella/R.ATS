f4ca4126ac6bbdcb537028f9e5b92b1b67b5dde3
Alex Sentinella <ATSentinella@gmail.com>
2022-01-13 15:43:46 +0100

    Changing cat_function to C.ATS for fun

---
+++ R/C.ATS.R
@@ -0,0 +1,22 @@
+#' A Cat Function -  To test out personal packages and functions
+#'
+#' This function tests your love of catsI
+#'
+#' When building your package, remember to make the documentation
+#' devtools::document()
+#'
+#'
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' C.ATS()
+
+C.ATS <- function(love=TRUE){
+  if(love==TRUE){
+    print("I love cats!")
+  }
+  else {
+    print("I do not love cats. That's OK.")
+  }
+}
